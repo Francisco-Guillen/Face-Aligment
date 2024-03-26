483da793c9fbd126680c65e27fe5dc7b9cf3ab13
Francisco Guillén <83434031+Francisco-Guillen@users.noreply.github.com>
2024-03-26 13:55:23 +0000

    Delete testar.py

--- testar.py
+++
@@ -1,9 +0,0 @@
-from LFWPairedDataset import LFWPairedDataset
-
-dataroot = 'Projeto/lfw-deepfunneled'
-pairs_cfg = 'Projeto/lfw-dataset/pairs.txt'
-#transform = ... # Defina suas transformações de imagem desejadas
-batch_size = 32 # Defina o tamanho do lote desejado
-
-dataset = LFWPairedDataset(dataroot, pairs_cfg)
-print(dataset.image_names_a)
